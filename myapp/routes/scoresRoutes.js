e8eff1bbabaf7ef8cf1298e89eac20ac2641e2fe
= <=>
2020-10-15 16:23:23 +0100

    Get + post request working

--- myapp/routes/scoresRoutes.js
+++ myapp/routes/scoresRoutes.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 //importing functions from scores 
 
 const {readAllData,getLeaderBoard,addToTable} = require("../model/scores");
 
 router.get("/", async function(req, res){
+  console.log("hello")
   const data = await readAllData();
   res.json({success: true, payload: data});
  });
